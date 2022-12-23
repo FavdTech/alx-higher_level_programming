67ab635d971dbed05b39c1f63ad972a963a30921
Angel <favouredangela@gmail.com>
2022-12-23 02:19:15 -0800

    task5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -25,11 +25,11 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+ 
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-    
+ 
     def my_print(self):
         """Print the square with the # character."""
         for i in range(0, self.__size):
